f3faa95218f202398ef57ec7a01779243ae6451c
ScreamZ <andreas.hanss67@gmail.com>
2015-01-28 17:28:16 +0100

    Improvement (cherry picked from commit 9ddfcb4)

--- src/metier/Catalogue.java
+++ src/metier/Catalogue.java
@@ -12,13 +12,14 @@ import java.util.*;
  */
 public class Catalogue implements I_Catalogue {
 
+    private final String SAVE_STRATEGY = "SQL"; // SQL or XML
     private String nom;
     private List<I_Produit> productList;
 
     private Catalogue() {
         try {
             this.productList = new ArrayList<>();
-            this.productList.addAll(ProduitDAOFactory.create("SQL").findAll());
+            this.productList.addAll(ProduitDAOFactory.create(SAVE_STRATEGY).findAll());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -52,7 +53,7 @@ public class Catalogue implements I_Catalogue {
         if (produit.getQuantite() < 0) return false;
         try {
             this.productList.add(produit);
-            ProduitDAOFactory.create("SQL").create(produit);
+            ProduitDAOFactory.create(SAVE_STRATEGY).create(produit);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -81,7 +82,7 @@ public class Catalogue implements I_Catalogue {
         if (p.getQuantite() < 0) return false;
         try {
             this.addProduit(p);
-            ProduitDAOFactory.create("SQL").create(p);
+            ProduitDAOFactory.create(SAVE_STRATEGY).create(p);
             return true;
         } catch (Exception e) {
             e.printStackTrace();
@@ -107,7 +108,7 @@ public class Catalogue implements I_Catalogue {
 
                 if (this.addProduit(p)) {
                     try {
-                        ProduitDAOFactory.create("SQL").create(p);
+                        ProduitDAOFactory.create(SAVE_STRATEGY).create(p);
                         nb++;
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -131,7 +132,7 @@ public class Catalogue implements I_Catalogue {
         for (I_Produit i_produit : productList) {
             if (i_produit.getNom().equals(nom)) {
                 try {
-                    ProduitDAOFactory.create("SQL").delete(i_produit);
+                    ProduitDAOFactory.create(SAVE_STRATEGY).delete(i_produit);
                     this.productList.remove(i_produit);
                     return true;
                 } catch (Exception e) {
@@ -159,7 +160,7 @@ public class Catalogue implements I_Catalogue {
             if (i_produit.getNom().equals(nomProduit)) {
                 try {
                     i_produit.ajouter(qteAchetee);
-                    ProduitDAOFactory.create("SQL").update(i_produit);
+                    ProduitDAOFactory.create(SAVE_STRATEGY).update(i_produit);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -186,7 +187,7 @@ public class Catalogue implements I_Catalogue {
                 try {
                     if (i_produit.getQuantite() < qteVendue) return false;
                     i_produit.enlever(qteVendue);
-                    ProduitDAOFactory.create("SQL").update(i_produit);
+                    ProduitDAOFactory.create(SAVE_STRATEGY).update(i_produit);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
