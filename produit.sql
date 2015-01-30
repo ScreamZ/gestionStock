590ce3f2abb392cd5d5f419a98dc25c9ed71227a
ScreamZ <andreas.hanss67@gmail.com>
2015-01-30 18:22:32 +0100

    Update SQL

--- produit.sql
+++ produit.sql
@@ -8,7 +8,8 @@ CREATE TABLE Produit
   CONSTRAINT unique_nom UNIQUE (nom)
 );
 CREATE SEQUENCE produit_seq;
-CREATE OR REPLACE PROCEDURE nouveauProduit(p_nomProduit     IN PRODUIT.NOM%TYPE, p_quantite IN PRODUIT.QUANTITE%TYPE,
+CREATE OR REPLACE PROCEDURE nouveauProduit(p_nomProduit IN PRODUIT.NOM%TYPE,
+                                           p_quantite   IN PRODUIT.QUANTITE%TYPE,
                                            p_prixUnitaireHT IN PRODUIT.PRIX_UNITAIRE_HT%TYPE)
 IS
   BEGIN
